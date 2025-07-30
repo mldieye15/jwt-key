431472a12d2abad7604d0f8ae9e7f8c387d5e4f0
mldieye15 <gaindedeslions@gmail.com>
2025-07-30 09:58:47 +0000

    Sauvegarde bis

--- src/main/java/sn/ucad/jwtkey/config/SecurityConfiguration.java
+++ src/main/java/sn/ucad/jwtkey/config/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 //package sn.ucad.jwtkey.config;
@@ -37,6 +38,7 @@ import org.springframework.security.web.SecurityFilterChain;
 //
 @Configuration
 @EnableMethodSecurity(securedEnabled = true)
+@EnableWebSecurity
 public class SecurityConfiguration {
 
     @Bean
